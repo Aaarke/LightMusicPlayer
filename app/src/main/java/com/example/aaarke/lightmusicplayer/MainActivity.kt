03a222d15bcdf0eae148f2f1ea581d53c60b6b0a
Roshan kumar <roshankumarroshi@gmail.com>
2019-11-26 22:45:15 +0530

    bug fixed in music player

--- app/src/main/java/com/example/aaarke/lightmusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/aaarke/lightmusicplayer/MainActivity.kt
@@ -9,11 +9,19 @@ import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import java.lang.Exception
+import java.util.*
+import android.media.RingtoneManager
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity(),SensorEventListener {
     var sensor:Sensor?=null
     var sensorManager:SensorManager?=null
-    var isRunning: Boolean? = null
+    var isRunning: Boolean? = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,20 +45,28 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         sensorManager?.unregisterListener(this)
     }
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if(event!!.values[0]>40&&isRunning==false){
+        var mp:MediaPlayer=MediaPlayer()
+        if(event!!.values[0]>0&&isRunning==false){
             //Play music player
             isRunning=true
             try {
-                var mp:MediaPlayer=MediaPlayer()
-                mp.setDataSource("http://2017.downloadming11.me/bollywood%20mp3/Simmba%20(2018)/Simmba%20(2018)%20(320%20Kbps)/01%20-%20Aankh%20Marey%20-%20DownloadMing.Se.mp3")
+                val alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+                mp.setDataSource(this,alert)
                 mp.prepare()
                 mp.start()
             }catch (e:Exception){
+                isRunning=false
 
             }
+        }else{
+            if (isRunning!!){
+                mp.stop()
+            }
+
         }
     }
 }
